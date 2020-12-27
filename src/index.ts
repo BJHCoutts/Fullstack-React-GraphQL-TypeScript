d20fd66381f09b4031d68a34caadbd8fffab2e56
Brian Coutts <bjhcoutts@gmail.com>
2020-12-26 17:32:41 -0800

    first mikroORM post

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log("hello world doop")
+import { MikroORM } from '@mikro-orm/core'
+import { __prod__ } from './constants'
+import { Post } from './entities/Post'
+
+const main = async () => {
+	const orm = await MikroORM.init({
+		entities: [Post],
+		dbName: 'lireddit',
+		// user: '',
+		// password: '',
+		type: 'postgresql',
+		debug: !__prod__,
+		// debug: true,
+	})
+
+	const post = orm.em.create(Post, { title: 'my first post'})
+
+	orm.em.persistAndFlush(post)
+	
+}
+
+main()
+
+console.log("hello world test")

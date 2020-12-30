b319a95be4ecbc6834fb1abc981be9b14e33d10f
Brian Coutts <bjhcoutts@gmail.com>
2020-12-30 15:59:12 -0800

    updated config options

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -2,8 +2,13 @@ import { __prod__ } from "./constants"
 import { Post } from "./entities/Post"
 import { MikroORM } from '@mikro-orm/core'
 import path from 'path'
+// import { postGreSQLUser, postGreSQLPass} from './hidden'
 
 export default {
+	// user: 'postgres',
+	// password: 'postgres',
+	// user: postGreSQLUser,
+	// password: postGreSQLPass,
 	migrations: {
     path: path.join(__dirname, './migrations'), // path to the folder with migrations
     pattern: /^[\w-]+\d+\.[tj]s$/, // regex pattern for the migration files
